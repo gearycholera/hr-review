f32caadee0e406d48f342068a6815992ffd388bc
girikalra <girikalra@gmail.com>
2018-09-07 16:08:50 -0700

    underbar - every

--- underbar-review/src/underbar.js
+++ underbar-review/src/underbar.js
@@ -211,6 +211,15 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+    return _.reduce(collection, function(foundFalse, item) {
+      if (!foundFalse) {
+        return false;
+      }
+      return (iterator(item) ? true : false)
+    }, true)
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
