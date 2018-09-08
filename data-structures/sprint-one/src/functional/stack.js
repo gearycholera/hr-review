1b648bf5ab1c81d283d69e6cc27f10d2589060fd
girikalra <girikalra@gmail.com>
2018-09-07 17:00:25 -0700

    data-structures - functional stack

--- data-structures/sprint-one/src/functional/stack.js
+++ data-structures/sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    var popped = storage[count - 1];
+    delete storage[count - 1];
+    count--;
+    if (count < 0) count = 0;
+    return popped;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
