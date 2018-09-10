ad5983e179460d77df695152bd3db5e190c41d81
girikalra <girikalra@gmail.com>
2018-09-10 16:20:23 -0700

    data-structure - linkedList

--- data-structures/sprint-two/src/linkedList.js
+++ data-structures/sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var removed = this.head.value;
+    var newHead = this.head.next;
+    this.head = newHead;
+    return removed;
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
