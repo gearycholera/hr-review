50e5fbb5b27de0e6e8c32c6b39f65f4fb33298f8
girikalra <girikalra@gmail.com>
2018-09-10 15:16:36 -0700

    data-structure - prototypal queue

--- data-structures/sprint-one/src/prototypal/queue.js
+++ data-structures/sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.count = 0;
+  instance.lowestCount = 0;
+  instance.magnitude = 0;
+  instance.storage = {};
+  return instance
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  this.storage[this.count] = val;
+  this.count++;
+  this.magnitude++;
+}
+
+queueMethods.dequeue = function() {
+  var dequeued = this.storage[this.lowestCount];
+  delete this.storage[this.lowestCount];
+  this.lowestCount++;
+  this.magnitude - 1 < 0 ? this.magnitude = 0 : this.magnitude--;
+  return dequeued;
+}
+
+queueMethods.size = function() {
+  return this.magnitude;
+}
 
