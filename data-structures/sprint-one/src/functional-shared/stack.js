a02c0576ed9a603a72fb7febd574303d577a60bb
girikalra <girikalra@gmail.com>
2018-09-10 14:53:50 -0700

    data-structure - functional-shared stack

--- data-structures/sprint-one/src/functional-shared/stack.js
+++ data-structures/sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.count = 0;
+  extend(instance, stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+var extend = function(dest, src) {
+  for (var key in src) {
+    dest[key] = src[key];
+  }
+}
+
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    var popped = this.storage[this.count-1];
+    delete this.storage[this.count - 1]
+    this.count--;
+    if (this.count < 0) this.count = 0;
+    return popped;
+  },
+  size: function() {
+    return this.count;
+  }
+
+};
 
 
