7e129f9111c95316b8952c50ffa9d10fca3bbc46
girikalra <girikalra@gmail.com>
2018-09-10 18:29:26 -0700

    data-structure - hash table

--- data-structures/sprint-two/src/hashTable.js
+++ data-structures/sprint-two/src/hashTable.js
@@ -7,14 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var keyValue = [k, v]
+  if (this._storage.get(index)) {
+    this._storage.get(index).push(keyValue);
+  } else {
+    this._storage.set(index, []);
+    this._storage.get(index).push(keyValue);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+  var output;
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) output = bucket[i][1];
+  }
+  return output;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
