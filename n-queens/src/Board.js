203bc86bd7940dc2a424374ad20c9e5778340cbd
girikalra <girikalra@gmail.com>
2018-09-14 15:07:35 -0700

    n-queens - row conflicts

--- n-queens/src/Board.js
+++ n-queens/src/Board.js
@@ -79,12 +79,24 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var count = 0;
+      for (var i = 0; i < row.length; i++) {
+        count += row[i];
+      }
+      return count > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var conflict = false;
+      var size = this.get('n');
+      for (var i = 0; i < size; i++) {
+        if (this.hasRowConflictAt(i)) {
+          conflict = true;
+        }
+      }
+      return conflict;
     },
 
 
