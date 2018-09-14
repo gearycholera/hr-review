0ece828534a03cdfab7a61f6c8ef804c87f80833
girikalra <girikalra@gmail.com>
2018-09-14 14:09:48 -0700

    subclass dance party - moved style settings to blinky dancer

--- subclass-dance-party/src/blinkyDancer.js
+++ subclass-dance-party/src/blinkyDancer.js
@@ -5,6 +5,12 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function() {
+  var styleSettings = {
+    border: '10px solid red',
+    borderRadius: '10px',
+    position: 'absolute'
+  };
+  this.$node.css(styleSettings);
   Dancer.prototype.step.call(this);
   this.$node.toggle();
 };
