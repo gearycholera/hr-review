9c59fef04ff90905768273343056e34801576739
girikalra <girikalra@gmail.com>
2018-09-26 18:01:52 -0700

    react-components - finished walkthrough

--- react-components/app.js
+++ react-components/app.js
@@ -1,1 +1,41 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return(
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+)
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['apples', 'bananas', 'cucumbers']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
